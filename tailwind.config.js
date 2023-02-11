36eba4e236fd7599ebd03f3faf9393fafdfd35b3
Hendratara Pratama <Kofusu>
2023-02-11 10:29:04 +0700

    init: initial setup husky, lint-staged, taliwindcss, prettier, antd

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        customBlue: '#16ABF8',
+        customBlack: '#111111',
+        customWhite: '#E5E5E5',
+      },
+      boxShadow: {
+        customShadow: '0px 4px 10px rgba(0, 0, 0, 0.1)',
+      },
+    },
+  },
+  plugins: [],
+}
